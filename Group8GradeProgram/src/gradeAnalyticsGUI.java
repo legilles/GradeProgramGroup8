d5dbf6c74e072dc4591c7b51068fc82eba8750b2
Laura <Laura@DESKTOP-246R5L0>
2019-06-20 13:51:19 -0700

    Initial creation of GUI. Buttons don't do anything yet.

---
+++ Group8GradeProgram/src/gradeAnalyticsGUI.java
@@ -0,0 +1,80 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class gradeAnalyticsGUI {
+	public static void main(String[] args)
+	{
+		
+		JFrame screenFrame = new JFrame("Grade Analytics Program");
+		screenFrame.setSize(900,500); //sets frame size
+		screenFrame.setDefaultCloseOperation(screenFrame.EXIT_ON_CLOSE); //program will quit if user clicks "x"
+		screenFrame.setVisible(true); 
+	
+		//Creates Labels
+		JLabel programTitle = new JLabel("Grade Analytics Program");
+
+		
+		//Creates buttons
+		JButton inputFileButton = new JButton("Import File");
+		JButton seeStatisticsButton = new JButton("See Statistics");
+		JButton seeGradePercentilesButton = new JButton("See Grade Percentiles");
+		JButton makeChangesButton = new JButton("Make A Grade Change");
+		JButton createReportButton = new JButton("Create Grade Report");
+		JButton quitButton = new JButton("Close Program");
+		
+		//Sets colors of buttons
+		inputFileButton.setBackground(Color.yellow);
+		seeStatisticsButton.setBackground(Color.green);
+		seeGradePercentilesButton.setBackground(Color.red);
+		makeChangesButton.setBackground(Color.cyan);
+		createReportButton.setBackground(Color.orange);
+		quitButton.setBackground(Color.magenta);
+	
+		
+		//Panel to Hold Buttons
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+		
+		//Panel to hold all user components
+		JPanel userPanel = new JPanel();
+		userPanel.setLayout(new BorderLayout());
+		userPanel.add(programTitle, BorderLayout.NORTH);
+		
+		//Panel to show results
+		JPanel showResultsPanel = new JPanel();
+		showResultsPanel.setBackground(Color.white);
+		userPanel.add(showResultsPanel,BorderLayout.CENTER);
+		
+		
+		//Adds buttons to button panel
+		buttonPanel.add(Box.createVerticalStrut(30));
+		buttonPanel.add(inputFileButton);
+		buttonPanel.add(Box.createVerticalStrut(30)); //adds spacing
+		buttonPanel.add(seeStatisticsButton);
+		buttonPanel.add(Box.createVerticalStrut(30));
+		buttonPanel.add(makeChangesButton);
+		buttonPanel.add(Box.createVerticalStrut(30));
+		buttonPanel.add(seeGradePercentilesButton);
+		buttonPanel.add(Box.createVerticalStrut(30));
+		buttonPanel.add(createReportButton);
+		buttonPanel.add(Box.createVerticalStrut(30));
+		buttonPanel.add(quitButton);
+		
+		userPanel.add(buttonPanel,BorderLayout.WEST);
+		
+		//Adds components to screen frame
+		screenFrame.add(userPanel);
+	}
+}
+
