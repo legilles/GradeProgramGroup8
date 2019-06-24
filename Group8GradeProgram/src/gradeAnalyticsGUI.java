1a8edd999129ca69f56b65a290cd5345eac90df3
Laura <Laura@DESKTOP-246R5L0>
2019-06-24 16:25:44 -0700

    Added action listeners to buttons, along with additional prompts.

--- Group8GradeProgram/src/gradeAnalyticsGUI.java
+++ Group8GradeProgram/src/gradeAnalyticsGUI.java
@@ -1,11 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class gradeAnalyticsGUI 
@@ -28,7 +33,7 @@ public class gradeAnalyticsGUI
 		//Creates buttons
 		JButton inputFileButton = new JButton("Import File");
 		JButton seeStatisticsButton = new JButton("See Statistics");
-		JButton seeGradePercentilesButton = new JButton("Change Grade Distribution");
+		JButton changeGradeDistriButton = new JButton("Change Grade Distribution");
 		JButton makeChangesButton = new JButton("Make A Grade Change");
 		JButton createReportButton = new JButton("Create Grade Report");
 		JButton quitButton = new JButton("Close Program");
@@ -36,7 +41,7 @@ public class gradeAnalyticsGUI
 		//Sets colors of buttons
 		inputFileButton.setBackground(Color.yellow);
 		seeStatisticsButton.setBackground(Color.green);
-		seeGradePercentilesButton.setBackground(Color.red);
+		changeGradeDistriButton.setBackground(Color.red);
 		makeChangesButton.setBackground(Color.cyan);
 		createReportButton.setBackground(Color.orange);
 		quitButton.setBackground(Color.magenta);
@@ -57,7 +62,7 @@ public class gradeAnalyticsGUI
 		showResultsPanel.setBackground(Color.white);
 		userPanel.add(showResultsPanel,BorderLayout.CENTER);
 		
-		//Temporary indicator of gui layout ("Results go here label")
+		//Temporary indicator of GUI layout ("Results go here label")
 		showResultsPanel.add(resultsGoHere,BorderLayout.CENTER);
 		
 		//Adds buttons to button panel
@@ -66,7 +71,7 @@ public class gradeAnalyticsGUI
 		buttonPanel.add(Box.createVerticalStrut(30)); //adds spacing
 		buttonPanel.add(makeChangesButton);
 		buttonPanel.add(Box.createVerticalStrut(30));
-		buttonPanel.add(seeGradePercentilesButton);
+		buttonPanel.add(changeGradeDistriButton);
 		buttonPanel.add(Box.createVerticalStrut(30));
 		buttonPanel.add(seeStatisticsButton);
 		buttonPanel.add(Box.createVerticalStrut(30));
@@ -76,8 +81,103 @@ public class gradeAnalyticsGUI
 		
 		userPanel.add(buttonPanel,BorderLayout.WEST);
 		
-		//Adds components to screen frame
-		screenFrame.add(userPanel);
+		
+		
+		
+		
+		//JButton Action Listeners
+		
+		//Close Program JButton Action Listener
+		quitButton.addActionListener(new ActionListener()
+				{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("Close Program Button Clicked"); //to see if listener worked
+
+				JOptionPane.showMessageDialog(screenFrame, "Are you sure you want to quit?", "Close Program",JOptionPane.WARNING_MESSAGE);
+			}	
+			});
+		
+		//Create Report JButton Action Listener
+		createReportButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("Create Grade Report Button Clicked");
+				
+				JFrame createReportFrame = new JFrame("Create Grade Report");
+				createReportFrame.setSize(400,100); //sets frame size
+				createReportFrame.setDefaultCloseOperation(createReportFrame.DISPOSE_ON_CLOSE); //program will close "create report" window if user clicks "x"
+				createReportFrame.setVisible(true); 
+				
+				JPanel createReportPanel = new JPanel();
+				createReportPanel.setBackground(Color.orange);
+				
+				JTextField reportNameTextField = new JTextField(20);
+				
+				JLabel nameReportPrompt = new JLabel("Please Enter a Name for the Grade Report.");
+				
+				createReportPanel.add(nameReportPrompt,BorderLayout.NORTH);
+				createReportPanel.add(reportNameTextField,BorderLayout.SOUTH);
+				
+				createReportPanel.add(reportNameTextField); //adds report name text field to report panel
+				createReportFrame.add(createReportPanel); //adds panel to report window
+			}	
+		});
+		
+		//See Statistics JButton Action Listener
+				seeStatisticsButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						System.out.println("See Satistics Button Clicked");
+					}	
+				});
+		
+		//Change Grade Distribution JButton Action Listener
+				changeGradeDistriButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						System.out.println("Change Grade Distribution Button Clicked");
+					}	
+				});
+		
+		//Make a Grade Change JButton Action Listener
+				makeChangesButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						System.out.println("Make Grade Change Button Clicked");
+					}	
+				});
+		
+		
+		//Import File JButton Action Listener
+				inputFileButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						System.out.println("Import File Button Clicked");
+					}	
+				});
+		
+		
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				//Adds components to screen frame
+				screenFrame.add(userPanel);	
+	
 			}
 		});
 	}
